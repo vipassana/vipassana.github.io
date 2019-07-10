1035c8a6f743442cd60d08614c38939a75dbb6ab
vipassana <vipassana@nyu.edu>
2019-07-09 23:30:57 -0400

    Adding a mad

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,16 @@
+var store = [{
+        "title": "This Divided Island, Samanth Subramanian",
+        "excerpt":"Nobody knew at that time whether it was the army or the Tigers who did it. There was utter panic The quote refers to a gruesome Kattankudy mosque massacre, where over a hundred boys and men were rounded and killed by the LTTE in two mosques. It might as well...","categories": ["Book Reviews"],
+        "tags": ["Book Reviews"],
+        "url": "http://localhost:4000/book%20reviews/bkr-this-divided-island/"
+      },{
+        "title": "Sleepless Nights, Elizabeth Hardwick",
+        "excerpt":"The torment of personal relations. Nothing new there except in the disguise, and in the escape on the wings of adjectives One of the consolations that literature provides is a sense of belonging, for those who may lack it in the real world. I’ve been able to reason to myself...","categories": ["Book Reviews"],
+        "tags": ["Book Reviews"],
+        "url": "http://localhost:4000/book%20reviews/bkr-hardwick/"
+      },{
+        "title": "Cheap Transportation: MTC Bus",
+        "excerpt":"I read about Nashville’s Declaration of Transportation Independence and it made my heart swell with the fond recollection of how cheap, safe, public transport gave me my independence. Mayor Briley signs 'Declaration of Transportation Independence' for NashvilleFebruary 1, 2017In 2003, I moved from Doha, Qatar to Chennai, India. Up until...","categories": ["Blog Post","Transit"],
+        "tags": ["transit","bus","train","twitter"],
+        "url": "http://localhost:4000/blog%20post/transit/blog-unreserved-bus/"
+      },]
---
+++ maps.md
@@ -0,0 +1,7 @@
+---
+title: Maps
+layout: collection
+permalink: /maps/
+show_excerpts: true
+entries_layout: list
+---
